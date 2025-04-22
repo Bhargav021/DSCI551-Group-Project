adb314bd30094be00184d570dc6e49a18aed2ce5
Bhargav Limbasia <blimbasi@usc.edu>
2025-04-21 19:51:41 -0700

    last commit - hopefully. everything works fine for now

--- Data/create-table-chats.sql
+++
@@ -1,8 +0,0 @@
--- Create chats table
-CREATE TABLE chats (
-    id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id),
-    chat_name VARCHAR(255),
-    messages TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
